0ced1b0764bfa1579e70f473bd184a35c4f62303
Richard Cubia <98155138+Treusu@users.noreply.github.com>
2024-12-16 22:22:54 +0800

    Add files via upload

---
+++ RunQuad.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+class Quadrilateral {
+    public void showDescription() {
+        System.out.println("- is quadrilateral");
+    }
+}
+
+class Rectangle extends Quadrilateral {
+    public void showDescription() {
+        super.showDescription();
+        System.out.println("- has 4 right angles");
+    }
+}
+
+class Square extends Rectangle {
+    public void showDescription() {
+        super.showDescription();
+        System.out.println("- has 4 equal sides");
+    }
+}
+
+public class RunQuad {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Press R for Rectangle or S for Square: ");
+        char choice = scanner.next().charAt(0);
+
+        if (choice == 'R') {
+            Rectangle rectangle = new Rectangle();
+            System.out.println("A rectangle:");
+            rectangle.showDescription();
+        } else if (choice == 'S') {
+            Square square = new Square();
+            System.out.println("A square:");
+            square.showDescription();
+        } else {
+            System.out.println("Invalid choice.");
+        }
+    }
+}
